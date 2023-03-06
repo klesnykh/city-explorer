13af80b36bb6f78a89bdd7c92694b6ba2d91c012
Kirill Lesnykh <kirlesnykh@gmail.com>
2023-03-06 14:13:38 -0800

    just making it work

--- src/App.js
+++ src/App.js
@@ -3,21 +3,25 @@ import './App.css';
 
 class App extends React.Component {
 
-citySubmit = async (event) => {
-  event.preventDefault();
-  let state = await axios.get(`https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.cityName}&format=json`);
-}
+// citySubmit = async (event) => {
+//   event.preventDefault();
+//   let state = await axios.get(`https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.cityName}&format=json`);
+// }
 
 
-  return (
-    <>
-      <header>
-        <h1>'I\'m Alive!!'</h1>
-      </header>
-      <main></main>
-      <footer></footer>
-    </>  
-  );
+  render() {
+    return (
+      <>
+        <header>
+          <h1>'I\'m Alive!!'</h1>
+        </header>
+        <main></main>
+        <footer></footer>
+      </>
+    );
+  }
+
+
 }
 
 export default App;
