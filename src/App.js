7319599bdc6dfab65d79e7d1b3583c8b5a43f306
Kirill Lesnykh <kirlesnykh@gmail.com>
2023-03-06 13:59:09 -0800

    basic react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+
+citySubmit = async (event) => {
+  event.preventDefault();
+  let state = await axios.get(`https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.cityName}&format=json`);
+}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <h1>'I\'m Alive!!'</h1>
       </header>
-    </div>
+      <main></main>
+      <footer></footer>
+    </>  
   );
 }
 
