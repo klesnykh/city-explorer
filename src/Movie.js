660e78ac529471dadc5e81589afabd316af33d5f
Kirill Lesnykh <kirlesnykh@gmail.com>
2023-03-08 18:47:35 -0800

    added movieDataBase

---
+++ src/Movie.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+
+class Movie extends React.Component{
+
+  render(){
+    return(
+      <Col lg={3}>
+        <Card style={{width:'18rem'}}>
+          <Card.Title>{this.props.title}</Card.Title>
+          <Card.Img variant="top" src={this.props.img_url}/>
+          <Card.Text>Overview: {this.props.overview}</Card.Text>
+          <Card.Text>Average Votes: {this.props.average_votes}</Card.Text>
+          <Card.Text>Total Votes: {this.props.total_votes}</Card.Text>
+          <Card.Text>Popularity: {this.props.popularity}</Card.Text>
+          <Card.Text>Released On: {this.props.released_on}</Card.Text>
+        </Card>
+      </Col>
+    );
+  }
+}
+
+export default Movie;
